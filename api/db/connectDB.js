bd26adb34f809d5805c50ae67b9eebaec2732963
Ahmed-Basher1 <a7medbasher811@gmail.com>
2022-08-27 14:47:10 +0200

    add middleware and add routes

--- api/db/connectDB.js
+++ api/db/connectDB.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  console.log('Database connected');
+  mongoose.connection.on('connected', () => {
+    console.log('Monogo db connectd');
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.log('Monogo db disconnected');
+  });
   return mongoose.connect(url);
 };
 module.exports = connectDB;
--- api/server.js
+++ api/server.js
@@ -2,14 +2,46 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db/connectDB');
 const app = express();
+const authRoute = require('./routes/auth');
+const hotelsRoute = require('./routes/auth');
+const roomsRoute = require('./routes/auth');
+const usersRoute = require('./routes/auth');
 dotenv.config();
 
+app.use('/api/auth', authRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+app.use('/api/users', usersRoute);
+const port = process.env.PORT || 5000;
+
+
+
+
+// not found middleware
+app.use((req, res) => {
+    res
+      .status(404)
+      .json({ success: false, message: "your request url is NOT FOUND" });
+  });
+  
+  // error middleware
+  app.use((error, req, res, next) => {
+    let status = error.status || 500;
+    res
+      .status(status)
+      .json({ success: false, message: "Internal Error" + error });
+  });
+  
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is listening on port ${port}...`);
-    });
-  } catch (error) {}
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+
 start();
