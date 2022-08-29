9ec94e28fba4bc90dd59fe4a3a6ffe71e0320386
Ahmed-Basher1 <a7medbasher811@gmail.com>
2022-08-29 16:49:50 +0200

    add login function using access token and refersh token

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const User = require('../models/Users');
+const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const {
@@ -54,22 +55,80 @@ module.exports.register = async (req, res, next) => {
   }
 };
 
-module.exports.verifyEmail = async (req, res) => {
-  const { verificationToken, email } = req.body;
-  const user = await User.findOne({ email });
+module.exports.verifyEmail = async (req, res, next) => {
+  try {
+    const { verificationToken, email } = req.body;
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    throw new CustomError.UnauthenticatedError('Verification Failed');
+    if (!user) {
+      throw new CustomError.UnauthenticatedError('Verification Failed');
+    }
+
+    if (user.verificationToken !== verificationToken) {
+      throw new CustomError.UnauthenticatedError('Verification Failed');
+    }
+
+    (user.isVerified = true), (user.verified = Date.now());
+    user.verificationToken = '';
+
+    await user.save();
+
+    res.status(StatusCodes.OK).json({ msg: 'Email Verified' });
+  } catch (error) {
+    next(error);
   }
+};
+module.exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  if (user.verificationToken !== verificationToken) {
-    throw new CustomError.UnauthenticatedError('Verification Failed');
+    if (!email || !password) {
+      throw new CustomError.BadRequestError(
+        'Please provide email and password'
+      );
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    if (!user.isVerified) {
+      throw new CustomError.UnauthenticatedError('Please verify your email');
+    }
+    const tokenUser = createTokenUser(user);
+
+    // create refresh token
+    let refreshToken = '';
+    // check for existing token
+    const existingToken = await Token.findOne({ user: user._id });
+
+    if (existingToken) {
+      const { isValid } = existingToken;
+      if (!isValid) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+      }
+      refreshToken = existingToken.refreshToken;
+      attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+      res.status(StatusCodes.OK).json({ user: tokenUser });
+      return;
+    }
+
+    refreshToken = crypto.randomBytes(40).toString('hex');
+    const userAgent = req.headers['user-agent'];
+    const ip = req.ip;
+    const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+    await Token.create(userToken);
+
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+  } catch (error) {
+    next(error);
   }
-
-  (user.isVerified = true), (user.verified = Date.now());
-  user.verificationToken = '';
-
-  await user.save();
-
-  res.status(StatusCodes.OK).json({ msg: 'Email Verified' });
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, verifyEmail } = require('../controllers/authController');
+const {
+  register,
+  verifyEmail,
+  login,
+} = require('../controllers/authController');
 
 router.get('/', (req, res) => {
   res.send('Hello, this is auth');
 });
 router.post('/register', register);
+router.post('/login', login);
 router.post('/verify-email', verifyEmail);
 module.exports = router;
